3aef0ba190f4f6b4bbf7a728fdb2eca5b96907fc
mariam <mariam@MYPROBOOK>
2023-03-04 10:19:12 +0400

    Add project files.

---
+++ Factorial/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Factorial
+{
+    internal class Program
+    {
+        static int Fact(int num)
+        {
+            if(num == 1 && num == 0)
+            {
+                return 1;
+            }
+            while(num > 1)
+             return num * Fact(num - 1);
+            return 1;
+        }
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(Fact(5));
+        }
+    }
+}
+
+
+
+
+
+
+
+
